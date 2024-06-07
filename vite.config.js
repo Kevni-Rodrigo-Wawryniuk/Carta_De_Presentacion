72230086871af8cdbcb2f5f33302e484be33548d
Kevin <126299266+Kevni-Rodrigo-Wawryniuk@users.noreply.github.com>
2024-06-07 09:20:13 -0300

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://Kevni-Rodrigo-Wawryniuk.github.io/Carta_De_Presentacion"
 })
