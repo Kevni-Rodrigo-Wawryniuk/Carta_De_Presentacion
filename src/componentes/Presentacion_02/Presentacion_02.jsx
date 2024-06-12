69e8c463bb5556a9a0d93b2892dbe265f52e91da
Kevin <126299266+Kevni-Rodrigo-Wawryniuk@users.noreply.github.com>
2024-06-12 14:09:18 -0300

    sin_whatsap

--- src/componentes/Presentacion_02/Presentacion_02.jsx
+++ src/componentes/Presentacion_02/Presentacion_02.jsx
@@ -7,7 +7,6 @@ import instagram from '../images/Presentacion_01/logoinstagram.png';
 import github from '../images/Presentacion_01/logogithub.png';
 import linkedin from '../images/Presentacion_01/logo_Linkedin.png';
 import itch from '../images/Presentacion_01/logoitch.png';
-import whatsap from '../images/Presentacion_01/logo_Whatsapp.png';
 import discord from '../images/Presentacion_01/logoDiscord.png';
 
 
@@ -113,7 +112,6 @@ function Presentacion_02() {
                 <ul>
                     <li><a href="https://www.instagram.com/kevinwawryniuk/"> <img id='logosEnlaces' src={instagram} alt="" /> </a></li>
                     <li><a href="https://github.com/Kevni-Rodrigo-Wawryniuk?tab=repositories"> <img id='logosEnlaces' src={github} alt="" /> </a></li>
-                    <li> <a href="https://api.whatsapp.com/send/?phone=5493764582381&text&type=phone_number&app_absent=0" > <img id='logosEnlaces' src={whatsap} alt="logo whatsap" /></a></li>
                     <li><a href="https://discord.gg/mMdAMRSA"> <img id='logosEnlaces' src={discord} alt="Discord" /></a></li>
                     <li><a href="https://www.linkedin.com/in/kevin-rodrigo-wawryniuk-7932a020b/"> <img id='logosEnlaces' src={linkedin} alt="" /> </a></li>
                     <li><a href="https://kwawryniuk.itch.io"> <img id='logosEnlaces' src={itch} alt="" /> </a></li>
